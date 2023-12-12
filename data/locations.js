1f41543268790859f4e08dfe886a9ba56e1bc731
Enu Clinton <enudariusgodwill@gmail.com>
2023-12-12 19:53:51 +0100

    adding buildings pictures

--- data/locations.js
+++ data/locations.js
@@ -1,11 +1,32 @@
 export const locationDetails = [
     {
-        name: "naphi",
-        image: "4756",
+        name: "Nahpi",
+        image: "4756.jpg",
         tel: "695103082",
         desc:"School Of Engineering",
         direction: "North",
-        site: "uniba.cm",
-        id: "Naphi"
+        website: "nahpi.cm",
+        id: "naphi",
+        'image-alt':"nahpi-image"
     },
+    {
+        name: "Higher Teachers Training College",
+        image: "htttc.jpg",
+        tel: "",
+        desc:"Enset",
+        direction: "The training of technical education teachers for secondary abd high schools",
+        website: "htttcuniba.edu.com",
+        id: "htttc",
+        'image-alt':"htttc block"
+    },
+    {
+        name: "College Of Technology",
+        image: "COLTECH.jpg",
+        tel: "",
+        desc:"College of Technology is school under the university of Bamenda based on agriculture, business, entrepreneurship, technology and engineering",
+        direction: "North",
+        website: "coltech.uniba.cm",
+        id: "coltech",
+        'image-alt':"coltech office"
+    }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -2,19 +2,25 @@ import { locationDetails } from "../data/locations.js";
 
 const searchBtn = document.querySelector('.js-search-btn');
 
-searchBtn.addEventListener('click', ()=>{
-  const inputButton = document.querySelector('.js-search-bar');
-  const locationName = inputButton.value
-  displaySideBar(locationName);
-});
+function getInputValue(){
+  const inputButtonValue = document.querySelector('.js-search-bar').value;
+  return inputButtonValue;
+}
 
-function displaySideBar(locationName){
+  searchBtn.addEventListener('click', ()=>{
+    displaySideBar();
+  });
+
+
+function displaySideBar(){
+  getInputValue()
+  const locationName = getInputValue();
   locationDetails.forEach((location) => {
-    if(locationName.toLowerCase() === location.name){
+    if(locationName.toLowerCase() === location.name.toLocaleLowerCase() || locationName.toLowerCase() === location.id){
         const htmlCode = `<div class="location-card js-location-card">
         <figure>
           <div class="img-container">
-            <img class="location-img" src="images/${location.image}.jpg" alt="naphi-image">
+            <img class="location-img" src="/images/pics/${location.image}" alt="${location['image-alt']}">
             <button onclick="const sideBar = document.querySelector('.js-location-card');
             sideBar.remove();" class="delete-icon-btn js-delete-icon-btn"><img src="/images/icons/delete.svg" class="delete-icon"></button>
           </div>
@@ -31,32 +37,37 @@ function displaySideBar(locationName){
               <img src="/images/icons/resume_50px.png" alt="">
               <p class="location-description">${location.desc}</p>
             </div> 
-            <div>
+            <div class="tel-container">
               <img src="/images/icons/phone_50px.png" alt="">
               <p><a href="tel:${location.tel}" class="tel">${location.tel}</a></p>
             </div> 
-            <div>
+            <div class="website-container">
               <img src="/images/icons/globe_24px.png" alt="">
-              <p class="location-site"><a href="https://www.${location.site}">${location.site}</a></p>
+              <p class="location-site"><a href="https://www.${location.website}">${location.website}</a></p>
             </div>
          </div>  
         </figure>
         </div>`;
         document.querySelector('.js-map-container').innerHTML = htmlCode;
-    }
-    else{
-        alert('Not found');
+        if(location.tel === ''){
+          document.querySelector('.tel-container').remove();
+        }
+        else if(location.website === ''){
+          document.querySelector('.website-container').remove();
+        }
     }
 });
 }
 
-const pathButton = document.querySelectorAll('.building');
-
-pathButton.forEach(building =>{
-  building.addEventListener('click', ()=>{
+const inputButton = document.querySelector('.js-search-bar');
+inputButton.addEventListener('keydown', (event)=>{
+  if(event.key === 'Enter'){
     displaySideBar();
-  });
+  }
 });
+
+document.querySelectorAll('.building');
+
 
 
 
